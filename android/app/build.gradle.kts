4b157abe61571715a5c32ab56fc9178771c0197a
yash12345-ctrl <yashtomar12345y@gmail.com>
2026-08-15 21:17:52 +0530

    Update targetSdk to 35 for Google Play requirement

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -34,7 +34,7 @@ android {
         // For more information, see: https://flutter.dev/to/review-gradle-config.
 //        minSdk = flutter.minSdkVersion
         minSdk = flutter.minSdkVersion
-        targetSdk = 34
+        targetSdk = 35
         versionCode = flutter.versionCode
         versionName = flutter.versionName
 
